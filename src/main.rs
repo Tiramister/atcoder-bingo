b8d01782c4f5610e49f350efa2e4f151da23eb2c
Tiramister <nh111424@gmail.com>
2022-03-17 17:31:34 +0000

    implement difficulty fetcher

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use crate::bingo_generator::get_difficulties;
+use log::error;
+
+mod bingo_generator;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let difficulties = match get_difficulties().await {
+        Ok(msg) => msg,
+        Err(e) => return error!("Failed to fetch difficulties: {:?}", e),
+    };
+
+    println!("{}...", difficulties.chars().take(50).collect::<String>());
 }
